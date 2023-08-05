313559beb6c97a541c1c901ea398eb36748390de
Deepayan Nandy <deepayan.622@gmail.com>
2023-08-05 17:13:21 +0530

    5/8/23_2

--- routes/schedule.js
+++ routes/schedule.js
@@ -9,17 +9,17 @@ const mongodb=require("mongodb");
 router.post('/',verifie_token,async (req,res)=>{
     if (req.tokendata.UserType!="Admin") return res.status(500).json({message:"Access Pohibited!"})
     var starttime=addMinutes(new Date(req.body.startdatetime),330);
-    var endtime =addMinutes(starttime,(req.body.duration));
+    var endtime =addMinutes(starttime,(req.body.Duration));
     console.log(endtime);
     const servicePackage= new schedulemodel({
         clientname:req.body.clientname,
-        clientid:req.body.clientid,
+        clientid:req.body.Client,
         startdatetime:starttime,
         enddatetime:endtime,
         resource:req.body.resource,
-        titel:req.body.servicedetails+" for "+req.body.clientname+"("+req.body.personcount+") at "+req.body.startdatetime,
-        servicedetails:req.body.servicedetails,
-        duration:req.body.duration,
+        titel:req.body.SelectedService+" for "+req.body.clientname+"("+req.body.personcount+") at "+req.body.startdatetime,
+        servicedetails:req.body.SelectedService,
+        duration:req.body.Duration,
         personcount:req.body.personcount,
         reschedulecount:0,
 
@@ -42,6 +42,24 @@ router.get('/:id', getSchedule,(req,res)=>{
 router.get('/',async (req,res)=>{
     try{
         const allschedules=await schedulemodel.find()
+        res.json(allschedules)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//get all services
+router.get('/existing/:time&:service&:duration',async (req,res)=>{
+    console.log(req.params.service)
+    let starttime=addMinutes( new Date(req.params.time),330)
+    let endtime=addMinutes( starttime,req.params.duration)
+    try{
+        const allschedules=await schedulemodel.find({ 
+            resource:req.params.service,
+            startdatetime: { $gte: starttime, 
+                    $lte: endtime
+                  } 
+            })
         res.json(allschedules)
     }catch(error){
         res.status(500).json({message: error.message})
