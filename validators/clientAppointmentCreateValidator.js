bf08c8ee9c3662ede36582b901813293dbd24d88
Mrinal098 <77658650+Mrinal098@users.noreply.github.com>
2024-04-16 17:44:59 +0530

    add multiple appointments (#5)

--- validators/clientAppointmentCreateValidator.js
+++ validators/clientAppointmentCreateValidator.js
@@ -1,15 +1,13 @@
 import joi from "@hapi/joi";
 
 export const clientAppointmentCreateValidator = (data) => {
-  const schema = joi
-    .object()
-    .keys({
-      membershipId: joi.string(),
-      serviceId: joi.string().required(),
-      personCount: joi.number().required(),
-      startDateTime: joi.date().required(),
-      branch: joi.string().required(),
-      duration: joi.number().required(),
-    })
+  const schema = joi.array().items({
+    membershipId: joi.string().optional(),
+    serviceId: joi.string().required(),
+    personCount: joi.number().required(),
+    startDateTime: joi.date().required(),
+    branch: joi.string().required(),
+    duration: joi.number().required(),
+  });
   return schema.validate(data);
 };
