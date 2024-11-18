7ebcfe56b98e4560dd2eeed31b754ea2ce308f16
Deepayan Nandy <deepayan.622@gmail.com>
2024-11-19 02:26:04 +0530

    dashboard fix

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import verifyToken from "../validators/verifyToken.js";
 import Client from "../models/clientModel.js";
-
+import appointmentModel from "../models/apointmentModel.js";
 const router = Router();
 
 router.get("/", verifyToken, async (req, res) => {
@@ -10,7 +10,20 @@ router.get("/", verifyToken, async (req, res) => {
     }
     try {
         const clientCount = await Client.countDocuments();
-        res.status(201).json({ "clientCount": clientCount});
+        var start = new Date();
+        start.setHours(0,0,0,0);
+
+        var end = new Date();
+        end.setHours(23,59,59,999);
+        console.log(start, end)
+        const todaysAppointments= await appointmentModel.countDocuments({$and:
+          [{startDateTime:{
+             $gt: start,
+             $lt: end
+         }},{
+             isCancelled:false
+         }]})
+        res.status(201).json({ "clientCount": clientCount,"todaysAppointments":todaysAppointments});
       } catch (error) {
         res.status(500).json({ message: error.message });
       }
